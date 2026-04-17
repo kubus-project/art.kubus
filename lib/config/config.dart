fe81d3b0a8780b87b76338485a37fdbb31d2907c
Rok Černezel <rok@kubus.site>
2026-04-17 20:12:20 +0200

    chore: update app version and build number to 0.5.9+26041701

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -518,8 +518,8 @@ class AppConfig {
 class AppInfo {
   static const String appName = 'art.kubus';
   static const String version = '0.5.9';
-  static const int buildNumber = 26041603;
-  static const String buildDate = '2026-04-16';
+  static const int buildNumber = 26041701;
+  static const String buildDate = '2026-04-17';
 
   /// Get full version string
   static String get fullVersion => '$version+$buildNumber';
