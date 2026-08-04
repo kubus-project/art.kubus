2e46585739f97a6e74860460a237d1d0a086988a
Rok Černezel <83780476+kubus-project@users.noreply.github.com>
2026-08-04 17:09:44 +0200

    chore(release): prepare 0.7.3

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -617,15 +617,15 @@ class AppInfo {
   static const String appName = 'art.kubus';
   static const String version = String.fromEnvironment(
     'KUBUS_APP_VERSION',
-    defaultValue: '0.7.2',
+    defaultValue: '0.7.3',
   );
   static const int buildNumber = int.fromEnvironment(
     'KUBUS_BUILD_NUMBER',
-    defaultValue: 26072901,
+    defaultValue: 26080401,
   );
   static const String buildDate = String.fromEnvironment(
     'KUBUS_BUILD_DATE',
-    defaultValue: '2026-07-29',
+    defaultValue: '2026-08-04',
   );
 
   /// Get full version string
